18d6ed5e9b363306d2aec9afa726979bfe9cca30
kleilsonsantos <kdsdesign1@gmail.com>
2022-10-12 15:07:19 -0300

    new files

--- Generics/src/Application/Program.java
+++ Generics/src/Application/Program.java
@@ -1,28 +1,32 @@
 package Application;
 
-import Services.PrintService;
-
-import java.util.*;
-
-import static java.util.Locale.*;
-
+//import Services.PrintService;
+// import static java.util.Locale.*;
+// import java.util.ArrayList;
+// import java.util.InputMismatchException;
+// import java.util.List;
+// import java.util.Scanner;
 
 public class Program {
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
-        setDefault(US);
-        List<Integer> integers = new ArrayList<>();
-        try (Scanner scanner = new Scanner(System.in)){
-            PrintService<Double> doublePrintService = new PrintService<>();
-            System.out.print("How many values? ");
-            int var = scanner.nextInt();
-            for(int i = 0; i < var; i++ ){
-                var value = scanner.nextDouble();
-                doublePrintService.addValue(value);
-            }
-            doublePrintService.print();
-            System.out.println("First -> " + doublePrintService.first());
-        } catch (InputMismatchException | IndexOutOfBoundsException e) {
-            System.out.println(e.getMessage());
-        }
+        System.out.println("Hello World");
+        // setDefault(US);
+        // // List<Integer> integers = new ArrayList<>();
+        // try (Scanner scanner = new Scanner(System.in)) {
+        // // PrintService<Double> doublePrintService = new PrintService<>();
+        // System.out.print("How many values? ");
+        // int var = scanner.nextInt();
+        // for (int i = 0; i < var; i++) {
+        // var value = scanner.nextDouble();
+        // // doublePrintService.addValue(value);
+        // }
+        // // doublePrintService.print();
+        // System.out.println("First -> ");
+        // } catch (InputMismatchException | IndexOutOfBoundsException e) {
+        // System.out.println(e.getMessage());
+        // }
     }
 }
